02e7bcc120424e5fccd3fb7f9df1243e66c3b41b
Great Ezenna <ezennagreat@gmail.com>
2025-08-06 23:11:54 +0100

    added a move method to the Player Class, checks the direction, and updates the player's position, doing futher testing to see if it works

--- maze.js
+++ maze.js
@@ -22,10 +22,28 @@ class Player {
     }
   }
 
-  get;
+  move(direction) {
+    switch (direction) {
+        case "right":
+            this.position[1]++
+            break;
+        case "left":
+            this.position[1]--
+            break;
+        case "up":
+            this.position[0]--
+            break;
+        case "down":
+            this.position[0]++
+        default:
+            break;
+    }
+  }
 }
 
 let game = new Maze();
 let player = new Player(game);
+
+player.move("right")
 
 console.log(player);
