24f9921dbde41c0cdfd5e753998e2bb3286610e8
Great Ezenna <ezennagreat@gmail.com>
2025-08-06 23:05:36 +0100

    made the Player's maze property private

--- maze.js
+++ maze.js
@@ -8,18 +8,24 @@ class Maze {
       ["#", " ", " ", " "],
       ["#", "E", "#", "#"],
     ];
+
+    this.startingPoint = [0, 0];
   }
 }
 
 class Player {
+  #maze;
   constructor(maze) {
     if (maze instanceof Maze) {
-      this.maze = maze;
+      this.#maze = maze;
+      this.position = this.#maze.startingPoint;
     }
   }
+
+  get;
 }
 
 let game = new Maze();
 let player = new Player(game);
 
-console.log(player.maze);
+console.log(player);
