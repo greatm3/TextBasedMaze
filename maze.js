37dcd824e8f4961448fee4df00d185492672a301
Great Ezenna <ezennagreat@gmail.com>
2025-08-06 23:24:47 +0100

    wanted to add a gridInfo(width, and height info) prop to the Player cla, but found out, it makes more sense to be in the Maze class

--- maze.js
+++ maze.js
@@ -15,6 +15,7 @@ class Maze {
 
 class Player {
   #maze;
+  #gridInfo;
   constructor(maze) {
     if (maze instanceof Maze) {
       this.#maze = maze;
@@ -24,39 +25,37 @@ class Player {
 
   move(direction) {
     switch (direction) {
-        case "right":
-            this.position[1]++
-            break;
-        case "left":
-            this.position[1]--
-            break;
-        case "up":
-            this.position[0]--
-            break;
-        case "down":
-            this.position[0]++
-        default:
-            break;
+      case "right":
+        this.position[1]++;
+        break;
+      case "left":
+        this.position[1]--;
+        break;
+      case "up":
+        this.position[0]--;
+        break;
+      case "down":
+        this.position[0]++;
+      default:
+        break;
     }
   }
 
-  #validMove(direction) {
-    
-  }
+  #validMove(direction) {}
 }
 
 let game = new Maze();
 let player = new Player(game);
 
-player.move("right")
-player.move("left")
-player.move("up")
-player.move("right")
-player.move("down")
-player.move("left")
-player.move("left")
-player.move("down")
-player.move("down")
-player.move("right")
+player.move("right");
+player.move("left");
+player.move("up");
+player.move("right");
+player.move("down");
+player.move("left");
+player.move("left");
+player.move("down");
+player.move("down");
+player.move("right");
 
 console.log(player);
