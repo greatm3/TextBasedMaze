43a94cc2f48f8df415ea809cdc9587b890a5d6ae
Great Ezenna <ezennagreat@gmail.com>
2025-08-07 20:10:17 +0100

    Testing in main.js

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-const Maze = require("./Maze")
+const Maze = require("./Maze");
+const Player = require("./Player");
+
+let maze = new Maze();
+let player = new Player(maze);
+
+console.log(player.move("right"));
+console.log(player.move("down"));
+console.log(player.move("down"));
+console.log(player.move("left"));
+console.log(player.move("right"));
+console.log(player.move("right"));
+console.log(player.move("down"));
+console.log(player.move("down"));
+console.log(player.move("left"));
+console.log(player.move("left"));
+console.log(player.move("done"));
